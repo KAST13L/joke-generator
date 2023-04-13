1f919e425c98c971e0bfbeeeb12b2a8cc77978af
KAST13L <o4enbsmeshno@gmail.com>
2023-04-13 22:45:13 +0300

    fix: changing the logic and architecture of the reducer for a better understanding of the code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -11,7 +11,6 @@ export const App = () => {
             <CssBaseline/>
             <ErrorSnackbar/>
             <Header/>
-
             <Container className={s.app}>
                 <JokeCardsList/>
             </Container>
