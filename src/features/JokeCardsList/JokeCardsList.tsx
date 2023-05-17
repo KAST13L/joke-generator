374fff04154c94fdc0dc44a8775c029f4f47bc02
KAST13L <o4enbsmeshno@gmail.com>
2023-05-17 22:42:55 +0300

    fix: update hook useActions, add dependencies

--- src/features/JokeCardsList/JokeCardsList.tsx
+++ src/features/JokeCardsList/JokeCardsList.tsx
@@ -31,7 +31,7 @@ export const JokeCardsList = () => {
           disabled={status === STATUS.LOADING}
           onClick={() => fetchJokes()}
         >
-          LOAD MOREE
+          LOAD MORE
         </Button>
       </Box>
     </Box>
