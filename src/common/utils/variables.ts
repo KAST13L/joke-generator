632ea0d66248b9cc74ac55bd21405557d8c309fc
KAST13L <o4enbsmeshno@gmail.com>
2023-06-11 11:21:58 +0300

    fix: update hook useActions, add dependencies

--- src/common/utils/variables.ts
+++ src/common/utils/variables.ts
@@ -1,5 +1,5 @@
 export const FAVORITE = "favorite";
-export const MAX_FAVORITE_JOKES_COUNT = 14;
+export const MAX_FAVORITE_JOKES_COUNT = 10;
 export enum STATUS {
   IDLE = "idle",
   LOADING = "loading",
